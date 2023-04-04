20e97a5b93dc427066f8ec3593bef5528db8ee97
melis <zmelismeric@gmail.com>
2023-04-04 18:13:35 +0100

    add cloudflare images

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,25 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import Image from 'next/image'
+//import Image from 'next/image'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Image } from 'cloudinary-react';
+import React, { useRef, useEffect } from 'react';
 
 export default function Home() {
+    const videoRef = useRef(null);
+    const videoRef2 = useRef(null);
+    const videoRef3 = useRef(null);
+    const videoRef4 = useRef(null);
+
+  useEffect(() => {
+    videoRef.current.play();
+    videoRef2.current.play();
+    videoRef3.current.play();
+    videoRef4.current.play();
+  }, []);
   return (
     
     <div className={styles.container}>
@@ -22,36 +35,37 @@ export default function Home() {
           <Row> 
             <Col>
              <a href="/musicartworks">
-                <Image alt="rhino" width={320} height={320} className={styles.img} src="/home/rhinoai.gif"/>
+                <Image alt="rhino" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626289/rhinoai_xfspjv.gif"/>
              </a>
              <figcaption className={styles.caption} >Music Art with AI</figcaption>
              <br/>
               <a href="/intpaint">
-                <Image alt="intpaint" width={320} height={320} className={styles.img} src="/home/intpaint.png"/>
+                <Image alt="intpaint" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626236/intpaint_gmmhmp.png"/>
              </a>
              <figcaption className={styles.caption} >Interactive Painting</figcaption>
              <figcaption className={styles.caption} >CCI Course Project 2022</figcaption>
              <br/>
               <a href="/kaleidoscope">
-                <Image alt="kaleidoscope" width={320} height={320} className={styles.img} src="/home/kaleidoscope.gif"/>
+                <video ref={videoRef} height="auto" width="100%"  className={styles.img}  src="https://res.cloudinary.com/dfn3uxsf6/video/upload/v1680628152/Screen_Recording_2023-04-04_at_18.02.15_i1mpvn.mov" class="w-100 shadow-1-strong rounded mb-4" autoplay loop> </video>
              </a>
              <figcaption className={styles.caption}>Kaleidoscope Shader</figcaption>
              <figcaption className={styles.caption}>CCI Course Project 2021</figcaption>
              <br/>
              <a href="/starrynight">
-              <Image alt="Starry" width={320} height={320} className={styles.img} src="/home/starry.gif"/>
+              <video ref={videoRef2} height="auto" width="100%"  className={styles.img}  src="https://res.cloudinary.com/dfn3uxsf6/video/upload/v1680628169/Screen_Recording_2023-04-04_at_18.01.44_ceuunj.mov" class="w-100 shadow-1-strong rounded mb-4" autoplay loop> </video>
+
              </a>
              <figcaption className={styles.caption}>Interactive Starry Night</figcaption>
              <figcaption className={styles.caption}>CCI Course Project 2021</figcaption>
              <br/>
               <a href="/polylines">
-                <Image alt="ofpoly" width={320} height={320} className={styles.img} src="/home/Openframeworks Interactive Polylines.png"/>
+                <Image alt="ofpoly" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626215/Openframeworks_Interactive_Polylines_lysrul.png"/>
               </a>
               <figcaption className={styles.caption}>Webcam Interactive Polylines</figcaption>
               <figcaption className={styles.caption}>Openframeworks 2020</figcaption>
               <br/>
               <a href="/soundreactiveled">
-                <Image alt="led"  width={320} height={320} className={styles.img} src="/home/Sound Interactive Led Project.png"/>
+                <Image alt="led"  width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626228/Sound_Interactive_Led_Project_wva2qg.png"/>
               </a>
               <figcaption className={styles.caption}>Sound Interactive Led</figcaption>
               <figcaption className={styles.caption}>Arduino Project 2020</figcaption>
@@ -60,37 +74,37 @@ export default function Home() {
 
             <Col >
             <a href="/picstape">
-                <Image alt="picstape" width={320} height={320} className={styles.img} src="/home/picstape.gif"/>
+                <video ref={videoRef3} height="auto" width="100%"  className={styles.img}  src="https://res.cloudinary.com/dfn3uxsf6/video/upload/v1680626845/picstape_rfmuyc.mp4" class="w-100 shadow-1-strong rounded mb-4" autoplay loop> </video>
              </a>
              <figcaption className={styles.caption} >Spotify Playlist to Images [Picstape]</figcaption>
              <figcaption className={styles.caption} >CCI Final Project 2022</figcaption>
              <br/>
                 <a href="/boid">
-                  <Image alt="boid"  width={320} height={320} className={styles.img} src="/home/boid.gif"/>
+                  <Image alt="boid"  width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626207/boid_s5ljpz.gif"/>
               </a>
               <figcaption className={styles.caption}>Boid Simulation</figcaption>
               <figcaption className={styles.caption}>CCI Course Project 2022</figcaption>
               <br/>
               <a href="/connection">
-                <Image alt="connection" width={320} height={320} className={styles.img} src="/home/connection.gif"/>
+                <Image alt="connection" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626206/connection_k5wqqy.gif"/>
               </a>
               <figcaption className={styles.caption}>Connection</figcaption>
               <figcaption className={styles.caption}>CCI Course Project 2021</figcaption>
               <br/>
               <a href="/magritte">
-                <Image alt="pipe" width={350} height={350} className={styles.img} src="/home/pipe.png"/>
+                <Image alt="pipe" width={350} height={350} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626219/pipe_hjrxdx.png"/>
               </a>
               <figcaption className={styles.caption}>Recreated Treachery Of Images</figcaption>
               <figcaption className={styles.caption}>CCI Course Project 2021</figcaption>
               <br/>
               <a href="/pumpkin">
-                <Image alt="pumpkin" width={320} height={320} className={styles.img} src="/home/pumpkin.gif" autoplay/>
+                <Image alt="pumpkin" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626222/pumpkin_zi7ysk.gif" autoplay/>
               </a>
               <figcaption className={styles.caption}>Light Detecting Pumpkin</figcaption>
               <figcaption className={styles.caption}>CCI Physical Computing Course Project 2021</figcaption>
               <br/>
               <a href="/paintpolyline">
-                <Image alt="paint" width={320} height={320} className={styles.img} src="/home/paint.jpg"/>
+                <Image alt="paint" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626216/paint_msepe2.jpg"/>
               </a>
               <figcaption className={styles.caption}>Paint with Polylines</figcaption>
               <figcaption className={styles.caption}>Openframeworks 2020</figcaption>
@@ -99,7 +113,7 @@ export default function Home() {
 
             <Col>
                           <a href="/ganflowers">
-                <Image alt="flower" width={320} height={320} className={styles.img} src="/home/flower.gif"/>
+                <Image alt="flower" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626213/flower_e9kygj.gif"/>
               </a>
                <figcaption className={styles.caption}>AI Flowers</figcaption>
                <figcaption className={styles.caption}>CCI Course Project 2022</figcaption>
@@ -111,25 +125,25 @@ export default function Home() {
               <figcaption className={styles.caption}>CCI Advanced Environments Course Project 2022</figcaption>
               <br/>
               <a href="/rooms">
-                <Image alt="extra" width={320} height={320} className={styles.img}  src="/home/extra.gif" autoplay/>
+                <Image alt="extra" width={320} height={320} className={styles.img}  src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626274/extra_lpnc8b.gif" autoplay/>
               </a>
               <figcaption className={styles.caption}>3js Rooms</figcaption>
               <figcaption className={styles.caption}>CCI Course Project, Threejs, 2021</figcaption>
               <br/>
               <a href="/paintingstomesh">
-                <Image alt="vincent" width={320} height={320} className={styles.img} src="/home/vincent.gif" autoplay/>
+                <Image alt="vincent" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626249/vincent_wwepxt.gif" autoplay/>
               </a>
               <figcaption className={styles.caption}>Paintings To Mesh</figcaption>
               <figcaption className={styles.caption}>Openframeworks 2020</figcaption>
               <br/>
               <a href="/arduinfeedback">
-                <Image alt="feedback" width={320} height={320} className={styles.img} src="/home/w3-l4-feedback.gif" autoplay/>
+                <Image alt="feedback" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626287/w3-l4-feedback_fmjfim.gif" autoplay/>
               </a>
               <figcaption className={styles.caption}>Arduino Feedback System</figcaption>
               <figcaption className={styles.caption}>CCI Advanced Physical Computing Course Project 2021</figcaption>
               <br/>
               <a href="/openglGame">
-                <Image alt="lab" width={320} height={320} className={styles.img} src="/home/lab.png"/>
+                <Image alt="lab" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626265/lab_rhm04q.png"/>
               </a>
               <figcaption className={styles.caption}>Labyrinth Game</figcaption>
               <figcaption className={styles.caption}>OpenGL 2019-20</figcaption>
@@ -138,31 +152,32 @@ export default function Home() {
 
             <Col>
               <a href="/wetmixer">
-                <Image alt="wtmxer" width={320} height={320} className={styles.img} src="/home/wtmxer.gif"/>
+                <Image alt="wtmxer" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626262/wtmxer_k25ecn.gif"/>
               </a>
               <figcaption className={styles.caption}>Wetmixer</figcaption>
               <figcaption className={styles.caption}>CCI Physical Computing Course Project 2021</figcaption>
               <br/>
               <a href="/spaceship">
-                <Image alt="space" width={320} height={320} className={styles.img} src="/home/space.gif"/>
+                <Image alt="space" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626235/space_lkmlf5.gif"/>
               </a>
               <figcaption className={styles.caption}>Spaceship</figcaption>
               <figcaption className={styles.caption}>CCI Course Project, Threejs, 2021</figcaption>
               <br/>
               <a href="/blender">
-                <Image alt="cloud" width={320} height={320} className={styles.img} src="/home/clouds2.png"/>
+                <Image alt="cloud" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626266/clouds2_oujocl.png"/>
               </a>
               <figcaption className={styles.caption}>Blender Projects</figcaption>
               <figcaption className={styles.caption}>2022</figcaption>
               <br/>
               <a href="/cleanwallet">
-                <Image alt="wallet" width={320} height={320} className={styles.img} src="/home/wallet.jpg"/>
+                <Image alt="wallet" width={320} height={320} className={styles.img} src="https://res.cloudinary.com/dfn3uxsf6/image/upload/v1680626251/wallet_fvzl3x.jpg"/>
               </a>
               <figcaption className={styles.caption}>Clean Wallet</figcaption>
               <figcaption className={styles.caption}>SU Physical Computing Course Project 2020</figcaption>
               <br/>
               <a href="/openframeworks">
-                <Image alt="pink" width={320} height={320} className={styles.img} src="/home/pink.gif" autoplay/>
+                <video ref={videoRef4} height="auto" width="100%"  className={styles.img}  src="https://res.cloudinary.com/dfn3uxsf6/video/upload/v1680628073/Screen_Recording_2023-04-04_at_18.01.02_elrjs2.mov" class="w-100 shadow-1-strong rounded mb-4" autoplay loop> </video>
+
               </a>
               <figcaption className={styles.caption}>Geometric Loops</figcaption>
               <figcaption className={styles.caption}>Openframeworks Basics 2020</figcaption>
