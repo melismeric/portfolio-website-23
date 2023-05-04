cb950fe1fca084b112e9902a42e4928d0d0e0d00
melis <zmelismeric@gmail.com>
2023-05-04 13:49:48 +0100

    Update musicartworks.js

--- pages/musicartworks.js
+++ pages/musicartworks.js
@@ -58,9 +58,9 @@ export default function Home() {
           </Col>
 
           <Col xs={12} sm={6} md={6}>
-            <iframe width="560" height="315"  src="https://www.youtube.com/embed/od2eJIdL46w" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/LbebSCRkho8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-            <iframe width="560" height="315"  src="https://www.youtube.com/embed/VRGdcQj97uU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+            <iframe width="100%"height="315"  src="https://www.youtube.com/embed/od2eJIdL46w" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+            <iframe width="100%"height="315" src="https://www.youtube.com/embed/LbebSCRkho8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+            <iframe width="100%" height="315"  src="https://www.youtube.com/embed/VRGdcQj97uU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
           </Col>
 
         </Row>
